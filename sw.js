a99df2692a768b6067a24cd7ba542d743850d362
teste352725-dev <teste352725@gmail.com>
2026-02-15 16:49:23 -0300

    feat(branding): atualizar ícones, og:image e logo para nova arte

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const urlsToCache = [
   'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js',
-  'https://i.ibb.co/mr93jDHT/JM.png'
+  'https://i.supaimg.com/24744ff6-247c-4e17-9e94-6455c96da980/dc727847-25ce-4e3b-97a3-7c26bb9493df.jpg'
 ];
 
 self.addEventListener('install', (event) => {
